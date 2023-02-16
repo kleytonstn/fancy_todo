c2d5ebb6554fd85ab3799f75b31e8c88fd5eb847
Alison Caique <alisonstark@academico.ufs.br>
2023-02-16 19:48:10 -0300

    Commit 001, updating the Padding widget to avoid code repetition;

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: Home(),
+    ),
+  );
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  // returns the home screen as Scaffold for hot reload
-        appBar: AppBar(
-            title: const Text('TODO App'),
-            backgroundColor: Colors.blue[100]
-
-        ),
-        body: ListView(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.all(8.0), // todo: how do add padding to the top of 1st container?
-                  child: Container(
-                    height: 50,                       // todo: can this repeated code be avoided?
-                    color: Colors.blue[100],
-                    alignment: const Alignment(-0.9, 0),
-                      child: const Text('#1 task'),
-
-                  )
-              ),   // offset from top and bottom
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(                   // Takes the task?
-                    height: 50,
-                    color: Colors.blue[100],
-                    alignment: const Alignment(-0.9, 0),
-                      child: const Text('#2 task'),
-                  )
-              ),
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(                   // Takes the task?
-                    height: 50,
-                    color: Colors.blue[100],
-                    alignment: const Alignment(-0.9, 0),
-                      child: const Text('#3 task'),
-                  )
-              ),
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(                   // Takes the task?
-                    height: 50,
-                    color: Colors.blue[100],
-                    alignment: const Alignment(-0.9, 0),
-                      child: const Text('#4 task'),
-                  )
-              )
-
-            ],
-          ),
-        // todo list will be displayed in here
+    final tasks = List.generate(5, (int index) => index);
+    return Scaffold(
+      // returns the home screen as Scaffold for hot reload
+      appBar: AppBar(
+          title: const Text('TODO App'), backgroundColor: Colors.blue[100]),
+      body: ListView(
+        children: tasks.map((task) => SomeKindaWankyWidget(taskName: '#$task task')).toList(),
+      ),
+      // todo list will be displayed in here
     );
   }
 }
 
+class SomeKindaWankyWidget extends StatelessWidget {
+  const SomeKindaWankyWidget({
+    Key? key,
+    required this.taskName,
+  }) : super(key: key);
+  final String taskName;
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        // Takes the task?
+        height: 50,
+        color: Colors.blue[100],
+        alignment: const Alignment(-0.9, 0),
+        child: const Text('#4 task'),
+      ),
+    );
+  }
+}
